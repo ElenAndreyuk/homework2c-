da553c3360e1e81cc87714182dd2e4bdb65a8676
elena <femtochka@yahoo.com>
2022-08-10 16:16:13 +0300

    первая задача

---
+++ task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показывает вторую цифру из введенного трехзначного числа
+Console.WriteLine("Введите трехзначное число ");
+int number =Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99) & (number < 1000))
+{
+   int a = number / 10;
+   int b = a % 10;
+   Console.WriteLine(b);
+
+}
+else
+Console.WriteLine("ошибка");
+
+
+

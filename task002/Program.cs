bfd6cee95024ac2c8fca46ace17c682f7a638612
elena <femtochka@yahoo.com>
2022-08-10 18:01:49 +0300

    вторая задача

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,6 +1,20 @@
 ﻿// Вывод третьей цифры из числа
 Console.WriteLine("Введите число");
-int number =Convert.ToInt32(Console.ReadLine());
+string numberString =Console.ReadLine();
 
-int numberLength = number.Length;
+int number =Convert.ToInt32(numberString);
+
+if ((number < -99) ^ (number > 99))
+{
+    char third =numberString[2];
+    Console.WriteLine(third);
+
+}
+else
+{
+    Console.WriteLine("Такой цифры нет");
+}
+
+
+
 

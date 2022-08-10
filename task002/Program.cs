987750cad6546bcec976652a7c02af0c38b76463
elena <femtochka@yahoo.com>
2022-08-10 16:58:02 +0300

    третья задача

---
+++ task002/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывод третьей цифры из числа
+Console.WriteLine("Введите число");
+int number =Convert.ToInt32(Console.ReadLine());
+
+int numberLength = number.Length;
+
---
+++ task003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Соответствует ли цифра выходному дню
+Console.WriteLine("Введите число от 1 до 7");
+int day =Convert.ToInt32(Console.ReadLine());
+
+if((day == 6) ^ (day == 7))
+{
+    Console.WriteLine("выходной");
+}
+if((day < 6) & (day > 0))
+{
+    Console.WriteLine("не выходной");
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
